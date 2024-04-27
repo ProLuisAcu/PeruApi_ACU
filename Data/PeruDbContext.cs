c77674c262652599053631eeee742b5aac131e09
ELIUSXD <164690306+ProLuisAcu@users.noreply.github.com>
2024-04-27 17:47:56 -0500

    Agregar archivos de proyecto.

---
+++ Data/PeruDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PeruApi.Data;
+
+public partial class PeruDbContext : DbContext
+{
+    public PeruDbContext()
+    {
+    }
+
+    public PeruDbContext(DbContextOptions<PeruDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<People> People { get; set; }
+
+    public virtual DbSet<Product> Product { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-KSCIP1PS\\SQLSERVER;Database=PeruDB;User=sa;Pwd=123456789;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<People>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__People__3213E83F0F2448AB");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Dni).HasMaxLength(18);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Fullname).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Product__3213E83F22418BD0");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Descriptions).HasMaxLength(100);
+            entity.Property(e => e.IsActive).HasColumnName("isActive");
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(10, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Data/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeruApi.Data;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Descriptions { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+
+    public bool? IsActive { get; set; }
+}
